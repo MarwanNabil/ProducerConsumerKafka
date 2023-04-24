f0a020536d9c9e1694d9b10d121f775ab27bc4d6
Marwan ElDeep <Marwan.Nabil.ElDeep@gmail.com>
2023-04-24 12:47:50 +0200

    added getOfferById

--- consumer/src/controllers/index.ts
+++ consumer/src/controllers/index.ts
@@ -1,4 +1,6 @@
 import { OfferModel } from "../models/offers";
+import { ResponseError } from "../helpers/responseError";
+import { validationResult } from "express-validator";
 import express, { Request, Response, NextFunction } from "express";
 
 export const getOffers = async (
@@ -9,6 +11,28 @@ export const getOffers = async (
   try {
     const offers = await OfferModel.find().lean();
     return res.status(200).json({ offersCount: offers.length, offers: offers });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOfferById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new ResponseError("validation failed.");
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const offerId = req.body.offerId;
+    const offer = await OfferModel.findById(offerId).lean();
+    return res.status(200).json({ ...offer });
   } catch (err) {
     next(err);
   }
--- consumer/src/routes/index.ts
+++ consumer/src/routes/index.ts
@@ -1,7 +1,29 @@
 import express from "express";
-import { getOffers } from "../controllers/index";
+import { body } from "express-validator";
+import { getOffers, getOfferById } from "../controllers/index";
+import { OfferModel } from "../models/offers";
 
 const routes = express.Router();
+
+routes.get(
+  "/offer",
+  [
+    body("offerId")
+      .trim()
+      .isString()
+      .isLength({ min: 2, max: 50 })
+      .withMessage(
+        "please enter a valid offer id. from 2 to 50 characters long."
+      )
+      .custom((offerId, { req }) => {
+        return OfferModel.findById(offerId).then((offer) => {
+          if (offer) return true;
+          return Promise.reject("no offer with that id.");
+        });
+      }),
+  ],
+  getOfferById
+);
 
 routes.get("/offers", getOffers);
 
